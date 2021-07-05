937eab650e969a913948af223621b052e06ecda5
Raja Shanmugam <rajashanmugam@Rajas-MacBook-Air.local>
2021-07-05 15:18:53 +1000

    remove un used change

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueMeta from 'vue-meta'
 
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-Vue.use(VueMeta)
 
 
 new Vue({
